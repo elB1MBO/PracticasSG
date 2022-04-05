f54988cc7dfd1376959eff9ff2cb217a08e0b94e
elB1MBO <raulduran.lacimada@gmail.com>
2022-04-05 10:45:40 +0200

    reloj funcional

--- ejerciciosThree/Pendulo/Pendulo.js
+++ ejerciciosThree/Pendulo/Pendulo.js
@@ -12,9 +12,11 @@ class Pendulo extends THREE.Object3D {
         */
        this.hv = 4.0; //altura verde
        this.hr = 5.0; //altura rojo
+       this.hs = 10.0; //altura del pendulo secundario
 
         this.penduloPrincipal = this.createPenduloPrincipal();
-        this.penduloSecundario = this.createPenduloSecundario();
+
+        //this.penduloSecundario.position.y = this.guiControls.posicionSecundario;
 
         this.add(this.penduloPrincipal);
 
@@ -49,10 +51,13 @@ class Pendulo extends THREE.Object3D {
         //Traslado la parte inferior segun la longitud de la parte roja
         this.parteVerdeInf.position.y = -this.hr*this.guiControls.longitudRojo;
 
+        this.penduloSecundario = this.createPenduloSecundario();
+
         var pendulo = new THREE.Object3D();
         pendulo.add(this.parteVerdeSup);
         pendulo.add(this.parteRoja);
         pendulo.add(this.parteVerdeInf);
+        pendulo.add(this.penduloSecundario);
         pendulo.add(cilindro);
 
         return pendulo;
@@ -60,38 +65,76 @@ class Pendulo extends THREE.Object3D {
     }
 
     createPenduloSecundario(){
+        var material = new THREE.MeshStandardMaterial({color: 0x3B1753});
+        var materialCilindro = new THREE.MeshStandardMaterial({color: 0x832E03});
 
+        var geometria = new THREE.BoxGeometry(2, this.hs, 1);
+        var geometriaCilindro = new THREE.CylinderGeometry(0.5, 0.5, 0.5, 6);
+        //Transformo la geometria del cilindro
+        geometriaCilindro.rotateX(Math.PI/2);
+        geometriaCilindro.translate(0,-0.5,2);
+
+        //Transformar la geometria del pendulo
+        geometria.translate(0, -this.hs/2, 1.5);
+
+        this.penduloSec = new THREE.Mesh(geometria, material);
+        var cilindro = new THREE.Mesh(geometriaCilindro, materialCilindro); 
+
+        this.penduloSec.scale.y = this.guiControls.longitudSecundario;
+        this.penduloSec.rotation.z = this.guiControls.rotacionSecundario;
+
+        this.penduloSec.position.y = -this.hv/2;
+        cilindro.position.y = -this.hv/2;
+
+        var penduloS = new THREE.Object3D();
+        penduloS.add(this.penduloSec);
+        penduloS.add(cilindro);
+
+        return penduloS;
     }
 
     createGUI(gui, titleGUI){
         this.guiControls = {
             longitudRojo : 1.0,
             rotacionPenduloP : 0.0,
-
+            longitudSecundario : 1.0,
+            rotacionSecundario : 0.0,
+            posicionSecundario : 10,
 
             //Boton de reset
             reset : () => {
                 this.guiControls.longitudRojo = 1.0;
                 this.guiControls.rotacionPenduloP = 0.0;
+                this.guiControls.longitudSecundario = 1.0;
+                this.guiControls.rotacionSecundario = 0.0;
+                this.guiControls.posicionSecundario = 0.0;
             }
         }        
-
         //Crear las carpetas
         var folder1 = gui.addFolder('Primer péndulo');
         var folder2 = gui.addFolder('Segundo péndulo');
         var folderReset = gui.addFolder('Reset');
-
+        //Principal
         folder1.add (this.guiControls, 'longitudRojo', 1.0, 2.0, 0.1).name('Longitud: ').listen();
         folder1.add (this.guiControls, 'rotacionPenduloP', -0.5, 0.5, 0.1).name('Rotación: ').listen();
+        //Secundario
+        folder2.add (this.guiControls, 'longitudSecundario', 1.0, 2.0, 0.1).name('Longitud: ').listen();
+        folder2.add (this.guiControls, 'rotacionSecundario', -0.5, 0.5, 0.1).name('Rotacion: ').listen();
+        folder2.add (this.guiControls, 'posicionSecundario', 10, 90, 1).name('Posición: ').listen();
 
+        //Boton de reset
         folderReset.add(this.guiControls, 'reset').name('RESET');
-
     }
 
     update(){ 
+        //Pendulo principal
         this.parteRoja.scale.y = this.guiControls.longitudRojo;
         this.parteVerdeInf.position.y = -this.hr*this.guiControls.longitudRojo;
         this.penduloPrincipal.rotation.z = this.guiControls.rotacionPenduloP;
+        //Pendulo secundario
+        this.penduloSec.scale.y = this.guiControls.longitudSecundario;
+        this.penduloSec.rotation.z = this.guiControls.rotacionSecundario;
+        this.penduloSecundario.position.y = -this.guiControls.posicionSecundario * this.guiControls.longitudRojo/100;
     }
 
 }
---
+++ ejerciciosThree/Reloj/Reloj.js
@@ -0,0 +1,68 @@
+import * as THREE from "../libs/three.module.js"
+
+class Reloj extends THREE.Object3D {
+    constructor(gui, titleGUI){
+        super();
+
+        this.createGUI(gui, titleGUI);
+
+        //para añadir las 12 esferas que representan las horas, las añadimos en un bucle
+        for(var i=1; i<=12; i++){
+            var esferaV = this.createEsfera()
+            esferaV.rotation.y = (Math.PI/6)*i; //Cada esfera creada, rota respecto al eje y dependiendo de i
+            this.add(esferaV);
+        }
+
+        this.manecilla = this.createManecilla();
+        //this.manecilla.rotation.y -= 0.01*this.guiControls.velocidad;
+
+        this.add(this.manecilla);
+    }
+
+    createEsfera(){
+        var material = new THREE.MeshStandardMaterial({color: 0x17D20E});
+
+        var geometria = new THREE.SphereGeometry(1, 32, 32);
+
+        geometria.translate(15, 0, 0);
+
+        var esferaQuieta = new THREE.Mesh(geometria, material);
+
+        return esferaQuieta;
+    }
+   
+    createManecilla(){
+        var material = new THREE.MeshStandardMaterial({color: 0xF00509});
+        var geometria = new THREE.SphereGeometry(1, 32, 32);
+
+        geometria.translate(10, 0, 0);
+
+        var esferaM = new THREE.Mesh(geometria, material);
+        return esferaM;
+    }
+
+    createGUI(gui, titleGUI){
+        this.guiControls = {
+            velocidad : 1.0,
+
+           reset : () => {
+               this.guiControls.velocidad = 1.0;
+           }
+        }        
+        //Crear las carpetas
+        var folder = gui.addFolder('Primer péndulo');
+        var folderReset = gui.addFolder('Reset');
+
+        folder.add(this.guiControls, 'velocidad', -12.0, 12.0, 1.0).name('Velocidad: ').listen();
+
+        //Boton de reset
+        folderReset.add(this.guiControls, 'reset').name('RESET');
+    }
+
+    update(){ 
+        this.manecilla.rotation.y -= 0.01*this.guiControls.velocidad;
+    }
+
+}
+
+export {Reloj}
